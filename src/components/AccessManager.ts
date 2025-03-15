ff7a3d471ba97397b652f15e1b53c3a77b97bee8
Lucas Stranks <78281961+TwoBrake@users.noreply.github.com>
2025-03-14 18:10:04 -0700

    begin working on access manager class

---
+++ src/components/AccessManager.ts
@@ -0,0 +1,89 @@
+// Resources
+import {
+  AnySelectMenuInteraction,
+  ButtonInteraction,
+  CommandInteraction,
+  Interaction,
+  ModalSubmitInteraction,
+} from "discord.js";
+import config from "../../config/bot";
+
+// Types
+type Interactions =
+  | Interaction
+  | CommandInteraction
+  | ModalSubmitInteraction
+  | ButtonInteraction
+  | AnySelectMenuInteraction;
+
+type Function = () => void | (() => Promise<void>);
+
+// Interfaces
+interface AccessManagerProps {
+  botOwner?: boolean;
+  botDev?: boolean;
+  owner?: boolean;
+  roles?: string[];
+  users?: string[];
+  other?: boolean[];
+}
+
+export default class AccessManager {
+  private readonly interaction: Interactions;
+  private readonly options: AccessManagerProps;
+
+  constructor(interaction: Interactions, props: AccessManagerProps) {
+    this.interaction = interaction;
+    this.options = props;
+  }
+
+  /**
+   * Returns a boolean on whether or not the interaction has the permission.
+   * @returns {boolean} Whether or not it was successful.
+   */
+  public has(): boolean {
+    return this.runChecks();
+  }
+
+  /**
+   * Allows you to run a function if the authentication is successful.
+   * @param fn The function to run upon success.
+   */
+  public restrict(fn: Function): void {
+    if (this.runChecks()) {
+      fn();
+    }
+  }
+
+  /**
+   * Runs internal checks.
+   * @returns {boolean} Whether or not it was successful.
+   */
+  private runChecks(): boolean {
+    const user = this.interaction.user;
+    const guild = this.interaction.guild;
+
+    // * If not user is found, bail out early.
+    if (!user) return false;
+
+    const isBotOwner = user.id === config.owner;
+    const isDev = config.developers.includes(user.id);
+    const isOwner = user.id === guild?.ownerId;
+
+    // * If user is bot owner, then return early because they are the owner, they have access to everything!
+    if (this.options.botOwner && isBotOwner) return true;
+
+    // * If user is bot developer, they should have access to everything for testing purposes.
+    if (this.options.botDev && isDev) return true;
+
+    // * If user is guild owner, they should have access over everything.
+    if (this.options.owner && isOwner) return true;
+
+    if (this.options.roles) {
+      // if (this.options.roles.includes())
+    }
+
+    // * If all checks fail, return false.
+    return false;
+  }
+}
