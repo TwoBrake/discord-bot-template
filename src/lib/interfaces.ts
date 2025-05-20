4eac6a770e672d643ccbd162845b3833ef7699e2
Lucas Stranks <78281961+TwoBrake@users.noreply.github.com>
2025-05-19 23:06:55 -0700

    document interfaces more properly

--- src/lib/interfaces.ts
+++ src/lib/interfaces.ts
@@ -9,40 +9,52 @@ import {
   GatewayIntentBits,
 } from "discord.js";
 
-// * Used for a base for Rest API responses (we only need the ID).
+/** Used for a base for Rest API responses (we only need the ID). */
 export interface RestResponse {
+  /** The ID of the Rest Response. */
   id: string;
 }
 
-// * Used for defining what a commands structure looks like.
+/** The structure of a command. */
 export interface Command {
+  /** The slash command data, containing the name, description, etc. */
   data: SlashCommandBuilder | SlashCommandSubcommandsOnlyBuilder;
+  /** A function that defines what happens once the command is ran. */
   execute: (interaction: ChatInputCommandInteraction) => Promise<void>;
+  /** Custom options for the command. */
   options?: CommandOptions;
 }
 
-// * Used for defining what a command can take as their options.
+/** The structure of a command's options. */
 export interface CommandOptions {
-  developerOnly?: boolean; // Restricts the command to the owner ID provided in configuration.
-  ownerOnly?: boolean; // Restricts the command to the owner of the guild.
+  /** Whether or not the command is restricted to a bots developer (provided in the configuration). */
+  developerOnly?: boolean;
+  /** Whether or not the command is restricted to the bots owner. */
+  ownerOnly?: boolean;
+  /** A cooldown for the command in milliseconds. */
   cooldown?: number;
 }
 
-// * Used for defining what an events structure looks like.
+/** The structure of an event. */
 export interface Event {
+  /** The type of event (eg. member join). */
   event: keyof ClientEvents;
+  /** Whether the event should be disconnected after its first run or not. */
   once: boolean;
+  /** A function determining what should happen when the event is triggered. */
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   execute: (...args: any[]) => void | Promise<void>;
 }
 
-// * Used for defining extra options on the client.
+/** A modified client built for the Discord bot template, allowing for commands and cooldowns to be added to the client class. */
 export interface ExtendedClient extends Client {
+  /** A collection of commands. */
   commands: Collection<string, Command>;
+  /** A collection of cooldowns. */
   cooldowns: Collection<string, Collection<string, Date>>;
 }
 
-// * Used for defining the options of an access manager instance.
+/** The structure of an access manager. */
 export interface AccessManagerOptions {
   botOwner?: boolean;
   botDev?: boolean;
@@ -52,7 +64,7 @@ export interface AccessManagerOptions {
   other?: boolean[];
 }
 
-// * Used for the configuration.
+/** The structure of the configuration of the bot. */
 export interface Configuration {
   guild: string;
   owner: string;
